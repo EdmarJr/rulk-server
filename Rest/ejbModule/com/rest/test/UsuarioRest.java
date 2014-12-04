6dcb5084add031bc36c2f15eb10f3f97f5fbd7f5
Edmar Junior <edmarfagunde@gmail.com>
2014-12-04 09:18:27 -0200

    tudo certo

---
+++ Rest/ejbModule/com/rest/test/UsuarioRest.java
@@ -0,0 +1,23 @@
+package com.rest.test;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import entitys.Usuario;
+
+@ApplicationScoped
+@Path("/")
+public class UsuarioRest {
+	@GET
+	@Path("usuario")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario obterUsuarioMock() {
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setNome("teste");
+		return usuario;
+	}
+}
