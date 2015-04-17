b3719b6d827b243305f7f9e271dca435e7c2555b
Edmar <edmarfagunde@gmail.com>
2015-04-17 17:06:15 -0300

    implementando testes

---
+++ Rest-EJB/src/test/java/PrimeiroTest.java
@@ -0,0 +1,12 @@
+import org.jglue.cdiunit.CdiRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CdiRunner.class)
+public class PrimeiroTest {
+	@Test
+	public void teste() {
+		System.out.println("teste");
+
+	}
+}
