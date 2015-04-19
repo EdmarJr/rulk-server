b3be9f613e73e1cbc60a84b3da77d941df0e839f
EdmarJr <edmarfagunde@gmail.com>
2015-04-19 16:30:17 -0300

    arquillian parcialmente funcionando

---
+++ Rest-EJB/ejbModule/com/rest/business/TesteBusiness.java
@@ -0,0 +1,9 @@
+package com.rest.business;
+
+
+public class TesteBusiness {
+	public String incluir() {
+		return "teste";
+
+	}
+}
--- Rest-EJB/src/test/java/com/rest/business/UnidadeBusinessTest.java
+++ Rest-EJB/src/test/java/com/rest/business/UnidadeBusinessTest.java
@@ -1,11 +1,30 @@
 package com.rest.business;
-import org.junit.Test;
+import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
 public class UnidadeBusinessTest {
 	
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(TesteBusiness.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
+	@Inject
+	private TesteBusiness testeBusiness;
 	
 	@Test
 	public void incluirUnidade() {
-		System.out.println("teste");
+		Assert.assertEquals(testeBusiness.incluir(), "teste");
 	}
 }
